459b1318bfe8b048763f75f1677f22b030b54797
River-mao <dhmao0516@gmail.com>
2020-04-27 23:31:41 +0800

    boosting算法的更改：修改程序整的bug.测试正确率1.0

--- work5/机器学习-作业5-毛冬辉.py
+++ work5/机器学习-作业5-毛冬辉.py
@@ -20,15 +20,15 @@ def create_data():
     df = pd.DataFrame(iris.data, columns=iris.feature_names)
     df['label'] = iris.target
     df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
-    data = np.array(df.iloc[:100, [0, 1, -1]])
+    data = np.array(df.iloc[0:60, [0, 1, -1]])
     for i in range(len(data)):
         if data[i,-1] == 0:
             data[i,-1] = -1
     # print(data)
-    return data[:,:1], data[:,-1]
+    return data[:,1:2], data[:,-1]
 
 X, y = create_data()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=65)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=7)
 
 
 class BoostingTree(object):
@@ -57,13 +57,31 @@ class BoostingTree(object):
         #决策值的索引列表
         self.index_list = self.max_iteration*[0]
 
+    def print_init(self):
+        print("---------------------------------")
+        print("-----------初始化----------------")
+        print("---------------------------------")
+        print("数据量:{}".format(len(y_train)+len(y_test)))
+        print("训练数据量:{}".format(len(y_train)))
+        print("测试数据量:{}".format(len(y_test)))
+        print("D={}".format(self.D))
+
+    def print_each_train(self,iteration,min_error_index,):
+        print("-------------------------------------")
+        print("-------------第{}个决策树-------------".format(iteration+1))
+        print("-------------------------------------")
+        print("决策树桩：v = {}".format(self.v_list[min_error_index]))
+        print("权重：alpha = {}".format(self.alpha[iteration]))
+        print("权值向量：D={}".format(self.D))
+
     def G_x(self, X_data, v):
-        if X_data>v:
-            return 1
+        if X_data < v:
+            return 1.0
         else:
-            return -1
+            return -1.0
 
     def cacl_v_list(self,X_train,v_number=100):
+        #self.v_list = X_train
         x_min = np.min(X_train)
         x_max = np.max(X_train)
         self.v_list = np.linspace(x_min, x_max,v_number)
@@ -71,41 +89,46 @@ class BoostingTree(object):
     def update_D(self,alpha,X_train,y_train,index):
         Zm = 0.0
         for i in range(self.N):
-            Zm += self.D[i]*np.exp(float(alpha*y_train[i]*self.G_x(X_train[i],self.v_list[index])))
+            Zm += self.D[i]*np.exp(-alpha*y_train[i]*self.G_x(X_train[i],self.v_list[index]))
         for i in range(self.N):
-            self.D[i] = self.D[i]*np.exp(float(alpha*y_train[i]*self.G_x(X_train[i],self.v_list[index])))/Zm
+            self.D[i] = self.D[i]*np.exp(-alpha*y_train[i]*self.G_x(X_train[i],self.v_list[index]))/Zm
+
+    def CalcMinErrorAndInex(self,X_train,y_train):
+
+        min_error = 1000
+        min_error_index = 0
+
+        # 对v_list的每个v值进行迭代
+        # 求出最小的error与返回使error最小的v值在v_list中的index
+        for i in range(len(self.v_list)):
+            current_error = 0.00001
+            for j in range(self.N):
+                G_i_x = self.G_x(X_train[j], self.v_list[i])
+
+                if G_i_x != y_train[j]:
+                    current_error += self.D[j]
+
+            if current_error < min_error:
+                min_error = current_error
+                min_error_index = i
+
+        return min_error,min_error_index
 
     def train(self,X_train,y_train):
-        min_error = 1
-        iteration = 0
+        self.print_init()
+        #iteration的大小，决定分类树桩的个数
+        for iteration in range(self.max_iteration):
+            min_error, min_error_index = self.CalcMinErrorAndInex(X_train, y_train)
 
-        while(1):
-            if (min_error <= self.min_error) | (iteration>=self.max_iteration):
+            #如果当前误差小于可接受最小误差，结束学习
+            if (min_error <= self.min_error):
                 break
 
-            index = 0
-            current_error = 0.00001
-            min_error_index = 0
-            for i in range(len(self.v_list)):
-                for j in range(self.N):
-                    G_i_x = self.G_x(X_train[j],self.v_list[index])
-
-                    if G_i_x != y_train[i]:
-                        current_error += self.D[i]
-
-                if current_error<min_error:
-                    min_error = current_error
-                    min_error_index = index
-                index +=1
-
             #记录本次迭代分类最小误差对应的v的索引
-            self.index_list[iteration]=min_error_index
+            self.index_list[iteration]= min_error_index
             self.alpha[iteration] = 0.5*np.log((1-min_error)/min_error)
-            self.update_D(self.alpha[iteration],X_train,y_train,self.index_list[min_error_index])
-
-            iteration+=1
-
-
+            self.update_D(self.alpha[iteration],X_train,y_train,self.index_list[iteration])
+            self.print_each_train(iteration,min_error_index)
 
     def predict(self,X_test,y_test):
 
@@ -121,11 +144,11 @@ class BoostingTree(object):
         return right_num/len(y_test)
 
 
-boostinftree = BoostingTree(X_train,y_train,max_iteration = 10, min_error = 0.001)
-boostinftree.cacl_v_list(X_train,v_number=50)
+boostinftree = BoostingTree(X_train,y_train,max_iteration = 5, min_error = 0.001)
+boostinftree.cacl_v_list(X_train,v_number=5)
 boostinftree.train(X_train,y_train)
 accuracy = boostinftree.predict(X_test,y_test)
-print("the accuracy is {}".format(accuracy))
+print("测试正确率为：{}".format(accuracy))
 
 
 
